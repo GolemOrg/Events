5652e0826713c2ea7aa36e55fa60d51c69d8b8e2
Matthew Jordan <mattjordanbusiness@gmail.com>
2022-02-15 12:46:08 -0600

    Create implementation

---
+++ src/test/kotlin/events/EventsTest.kt
@@ -0,0 +1,43 @@
+package events
+
+import org.junit.jupiter.api.Test
+
+sealed class PlayerEvent {
+    object Join : PlayerEvent()
+    object Leave : PlayerEvent()
+    class Move(val x: Int, val y: Int) : PlayerEvent()
+}
+
+class EventsTest {
+
+    @Test
+    fun testPlayerEvent() {
+        val bus = EventBus<PlayerEvent>()
+        bus.listen(this) {
+            when (it) {
+                is PlayerEvent.Join -> println("Player joined")
+                is PlayerEvent.Leave -> println("Player left")
+                is PlayerEvent.Move -> println("Player moved to (${it.x}, ${it.y})")
+            }
+        }
+
+        bus.dispatch(PlayerEvent.Join) // they joined
+        for (i in 1..10) {
+            bus.dispatch(PlayerEvent.Move(i, i)) // they moved
+        }
+        bus.dispatch(PlayerEvent.Leave) // they left
+        bus.remove(this)
+
+    }
+
+    @Test
+    fun testInt() {
+        val bus = EventBus<Int>()
+        bus.listen { number -> println("Received number: $number") }
+
+        for(i in 1..10) {
+            bus.dispatch(i)
+        }
+    }
+
+}
